c85648bef529da0a712e9e4284e0983045fcced3
Gooblicraft <148702245+gooblicraft@users.noreply.github.com>
2026-08-09 21:38:11 +0800

    Added Charizard structures in basic state

---
+++ pokemon.py
@@ -0,0 +1,11 @@
+charizard = {
+  "pokemonHP": 700,
+  "pokemonDMG": {
+    "skill1": 70,
+    "skill2": 100,
+    "skill3": 125,
+    "run": "You Avoided this Pokemon . . ."
+  }
+}
+
+print(charizard)
