bb795b450241c5843388748392e3ffcc3be09321
Incineradi <incineradi@gmail.com>
2026-08-31 09:44:54 +0800

    made a separate skill class for pokemons to allow better scalability and better readablity than iterating through a list of values and to avoid falling into the nesting rabbithole lol (plus its cleaner). note: stun is not required to be implemented by the battle system.

--- pokemon.py
+++ pokemon.py
@@ -1,28 +1,43 @@
+class Skill:
+    def __init__(self, name, dmg, stun):
+        self.name=name
+        self.dmg=dmg
+        self.stun=stun
+
 class Pokemon: 
-    def __init__ (self, name, health, damage, skills):
+    def __init__ (self, name, health, skills):
+
         self.name=name
         self.health=health
-        self.damage=damage
         self.skills=skills
 
-    
-    def run(self):
-        print(f"{self.name} ran away!")
+##skill list
+#PIKACHU
+pika1=Skill("thunder fart", 99, 100)
+pika2=Skill("lightning whatever", 99, 100)
+pika3=Skill("i dont play pokemon attack", 99, 100)
 
-pikachu = Pokemon(
-    "Pikachu",
-    100,
-    1,
-    {
-        "thunder fart": 99,
-        "lightning strike": 1,
-        "lightning whatever": 23,
-        "i dont play pokemon attack": 100
-    }
-)
+pokedex={
+    "pikachu": Pokemon(
+        "Pikachu",
+        100,
+        [pika1, pika2, pika3]
+    ),
+}
 
-print(pikachu.name)
-print(pikachu.health)
-print(pikachu.damage)
-print(pikachu.skills)
-pikachu.run()
+p1 = pokedex["pikachu"]
+
+print(p1.name)
+print(p1.health)
+print("--------------------------")
+print(p1.skills[0].name)
+print(p1.skills[0].dmg)
+print(p1.skills[0].stun)
+
+print(p1.skills[1].name)
+print(p1.skills[1].dmg)
+print(p1.skills[1].stun)
+
+print(p1.skills[2].name)
+print(p1.skills[2].dmg)
+print(p1.skills[2].stun)
